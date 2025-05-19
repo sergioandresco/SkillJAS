45e71fd3f42067fdccf12bf2e2f512e3b0ad5a03
sergioandresco <sergiocobos6@outlook.com>
2025-05-18 21:12:55 -0500

    fix(adminVideos): create new component videoTable for add the pagination and load the all video data

--- src/hooks/useAdminVideos.js
+++ src/hooks/useAdminVideos.js
@@ -5,63 +5,63 @@ import { useAuth } from "@clerk/clerk-react";
 const ALL_VIDEOS_URL = import.meta.env.VITE_ADD_VIDEO;
 const STATUS_VIDEO_URL = import.meta.env.VITE_STATUS_COURSES;
 
-export function useAdminVideos() {
-  const queryClient = useQueryClient();
-  const { getToken } = useAuth(); // Clerk
+export function useAdminVideos(page = 1) {
+    const queryClient = useQueryClient();
+    const { getToken } = useAuth();
 
-  // 1. Obtener todos los videos
-  const { data, isLoading } = useQuery({
-    queryKey: ["videos"],
-    queryFn: async () => {
-      const response = await axios.get(ALL_VIDEOS_URL);
-      return response.data?.items || [];
-    },
-  });
+    const { data, isLoading, isFetching } = useQuery({
+        queryKey: ["videos", page],
+        queryFn: async () => {
+          const response = await axios.get(`${ALL_VIDEOS_URL}?Page=${page}`);
+          return response.data;
+        },
+        keepPreviousData: true,
+    });
 
-  // 2. Habilitar video
-  const enableMutation = useMutation({
-    mutationFn: async (id) => {
-      const token = await getToken();
-      return await axios.patch(
-        `${STATUS_VIDEO_URL}/${id}/enable`,
-        {},
-        {
-          headers: {
-            Authorization: `Bearer ${token}`,
-            "X-Role": "admin",
-          },
-        }
-      );
-    },
-    onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: ["videos"] });
-    },
-  });
+    const enableMutation = useMutation({
+      mutationFn: async (id) => {
+        const token = await getToken();
+        return await axios.patch(
+          `${STATUS_VIDEO_URL}/${id}/enable`,
+          {},
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+              "X-Role": "admin",
+            },
+          }
+        );
+      },
+      onSuccess: () => {
+        queryClient.invalidateQueries({ queryKey: ["videos"] });
+      },
+    });
 
-  // 3. Deshabilitar video
-  const disableMutation = useMutation({
-    mutationFn: async (id) => {
-      const token = await getToken();
-      return await axios.patch(
-        `${STATUS_VIDEO_URL}/${id}/disable`,
-        {},
-        {
-          headers: {
-            Authorization: `Bearer ${token}`,
-            "X-Role": "admin",
-          },
-        }
-      );
-    },
-    onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: ["videos"] });
-    },
-  });
+    const disableMutation = useMutation({
+      mutationFn: async (id) => {
+        const token = await getToken();
+        return await axios.patch(
+          `${STATUS_VIDEO_URL}/${id}/disable`,
+          {},
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+              "X-Role": "admin",
+            },
+          }
+        );
+      },
+      onSuccess: () => {
+        queryClient.invalidateQueries({ queryKey: ["videos"] });
+      },
+    });
 
-  return {
-    videos: data || [],
-    isLoading,
-    enableVideo: enableMutation.mutate,
-    disableVideo: disableMutation.mutate,
-  };
+    return {
+        videos: data?.items || [],
+        totalPages: data?.totalPages || 1,
+        isLoading,
+		isFetching,
+        enableVideo: enableMutation.mutate,
+        disableVideo: disableMutation.mutate,
+    };
 }
